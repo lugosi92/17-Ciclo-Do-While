70782a8c9c864d38ec9d74faec8a6b79b7ed20f6
khaled___beno <khaled___beno@localhost>
2023-12-26 19:30:48 +0100

    Do-While

---
+++ 17_Ciclo_Do-While/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*ciclos while, do-while,for, foreach
+		
+		int num = 0;
+		
+		while (num < 10) {
+			System.out.println("Hola mundo");
+			num++;
+		}
+	}*/
+		
+		//Caracteristicas principales de los ciclos
+		// FINIDUD -> finalizar
+		
+		int num  =0;
+		do{
+			System.out.println("Hola mundo");
+			num++;
+		}while( num < 10);
+	}
+	
+}
